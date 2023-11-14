4f016632311b336a9f5c8a0ba1bbdf05f0fb5e7b
imigoroliveira <igormat4us@gmail.com>
2023-11-13 22:27:28 -0300

    Fixing position header and footer settings, additing right color and fixing other reference's problems

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuartosComponent } from './pages/quartos/quartos.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'quartos', component: QuartosComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'funcionarios', component: FuncionariosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuartosComponent } from './pages/quartos/quartos.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    QuartosComponent,
+    ClientesComponent,
+    FuncionariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    NoopAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
